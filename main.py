af638c55a52118574d203d18c4662a44048fe8e2
WASI AHMED <84765303+wasiongit@users.noreply.github.com>
2022-10-16 23:50:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,9 @@ def main():
     number1=reader.readtext('number.png',detail=False)
     number2=reader.readtext('number2.png',detail=False)
     
+    print(number1+number2)
     return(number1+number2)
+
 
 if __name__ == '__main__':
     reader = easyocr.Reader(['en'])
